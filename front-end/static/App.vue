429463a2c04b6788c539f172be70ec282705c9c8
bwj <57085345+bwj124@users.noreply.github.com>
2020-12-09 01:06:46 +0800

    更新Vue

---
+++ front-end/static/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+      <vue-particles
+        color="#fff"
+        :particleOpacity="0.7"
+        :particlesNumber="60"
+        shapeType="circle"
+        :particleSize="4"
+        linesColor="#fff"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.4"
+        :linesDistance="150"
+        :moveSpeed="2"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+        class="lizi"
+      >
+      </vue-particles>
+    <router-view/>
+  </div>
+</template>
+ 
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
