7b8d42c23c9b8f406adc2c14b65d50d3c3b365b9
bwj124 <bwj_email@163.com>
2020-12-09 22:54:06 +0800

    更新App.vue

--- front-end/static/App.vue
+++ front-end/static/App.vue
@@ -1,30 +1,17 @@
 <template>
   <div id="app">
-      <vue-particles
-        color="#fff"
-        :particleOpacity="0.7"
-        :particlesNumber="60"
-        shapeType="circle"
-        :particleSize="4"
-        linesColor="#fff"
-        :linesWidth="1"
-        :lineLinked="true"
-        :lineOpacity="0.4"
-        :linesDistance="150"
-        :moveSpeed="2"
-        :hoverEffect="true"
-        hoverMode="grab"
-        :clickEffect="true"
-        clickMode="push"
-        class="lizi"
-      >
-      </vue-particles>
     <router-view/>
   </div>
 </template>
- 
+
 <script>
-  export default {
-    name: 'App'
-  }
+export default {
+  name: 'app'
+}
 </script>
+
+<style>
+@import 'normalize.css/normalize.css';
+@import './style/index.css';
+@import './style/custom.css';
+</style>
